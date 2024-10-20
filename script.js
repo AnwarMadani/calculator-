7f552337256b2adbeb326004bc26d5823f50041d
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-10-20 16:59:37 +0200

    add basic layout, style and functions for console

---
+++ script.js
@@ -0,0 +1,38 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+function operate(num1, num2, operator){
+    let result;
+    switch(operator){
+        case '+':
+        result = add(num1, num2);
+        break;
+        case '-':
+        result = subtract(num1, num2);
+        break;
+        case '*':
+        result = multiply(num1, num2);
+        break;
+        default:
+        result = divide(num1, num2);
+        break;
+    }
+    return result;
+}
