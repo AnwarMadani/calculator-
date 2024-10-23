42f667a6945b8885eae7c69d4f3a05b2ef3b7c28
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-10-23 13:59:33 +0200

    add clear and delete button logic

--- script.js
+++ script.js
@@ -6,9 +6,27 @@ let display = document.getElementById('currentOperationScreen');
 
 let equalBtn = document.getElementById('equalsBtn');
 
+let clearBtn = document.getElementById('clearBtn');
+
+let deleteBtn = document.getElementById('deleteBtn');
+
 numberButtons.forEach(button => button.addEventListener("click", (event)=> appendValue(event.target.textContent)))
 
 operatorButtons.forEach(button => button.addEventListener("click", (event)=> appendOperator(event.target.textContent)))
+
+clearBtn.addEventListener("click", clear);
+
+deleteBtn.addEventListener("click", deleteLastValue);
+
+function deleteLastValue(){
+    if(display.textContent !== ''){
+        display.textContent = display.textContent.toString().slice(0, -1)
+    }
+}
+
+function clear(){
+    display.textContent = '';
+}
 
 function appendValue(value){
     display.textContent += value;
@@ -27,14 +45,29 @@ function calculate(){
         let expression = display.textContent;
         if(expression.includes('+')){
             expression = expression.split('+');
-            console.log(expression)
             let firstOperand = expression[0];
-            console.log(firstOperand)
             let secondOperand = expression[1];
-            console.log(secondOperand)
             display.textContent = Number(firstOperand) + Number(secondOperand);
+        } else if(expression.includes('-')){
+            expression = expression.split('-');
+            let firstOperand = expression[0];
+            let secondOperand = expression[1];
+            display.textContent = Number(firstOperand) - Number(secondOperand);
+        } else if(expression.includes('*')){
+            expression = expression.split('*');
+            let firstOperand = expression[0];
+            let secondOperand = expression[1];
+            display.textContent = Number(firstOperand) * Number(secondOperand);
+        } else if(expression.includes('/')){
+            expression = expression.split('/');
+            let firstOperand = expression[0];
+            let secondOperand = expression[1];
+            if(secondOperand == 0) return;
+            display.textContent = Number(firstOperand) / Number(secondOperand);
+        } else {
+            return;
         }
-    }
+    } 
 }
 
 // append all pressed numbers in display if it doesn't already contain an operator and then split the display value, based on the operator, and the do the calculation and return it
