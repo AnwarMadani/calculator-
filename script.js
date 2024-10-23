1ad92e28be252e08b25881bb1b0a0b978ecd3d52
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-10-23 13:44:39 +0200

    add logic for addition

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let operatorButtons = document.querySelectorAll('[data-operator]');
 
 let display = document.getElementById('currentOperationScreen');
 
+let equalBtn = document.getElementById('equalsBtn');
+
 numberButtons.forEach(button => button.addEventListener("click", (event)=> appendValue(event.target.textContent)))
 
 operatorButtons.forEach(button => button.addEventListener("click", (event)=> appendOperator(event.target.textContent)))
@@ -15,6 +17,23 @@ function appendValue(value){
 function appendOperator(operator){
     if(!isNaN(Number(display.textContent))){
         display.textContent += operator;
+    }
+}
+
+equalBtn.addEventListener("click", calculate);
+
+function calculate(){
+    if((display.textContent !== '') && (isNaN(Number(display.textContent)))){
+        let expression = display.textContent;
+        if(expression.includes('+')){
+            expression = expression.split('+');
+            console.log(expression)
+            let firstOperand = expression[0];
+            console.log(firstOperand)
+            let secondOperand = expression[1];
+            console.log(secondOperand)
+            display.textContent = Number(firstOperand) + Number(secondOperand);
+        }
     }
 }
 
